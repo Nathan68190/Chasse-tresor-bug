7ed9937ee7b5d4e927c58b2de5fc6da9be899c83
Nathan68190 <55447407+Nathan68190@users.noreply.github.com>
2021-07-05 18:02:43 +0200

    Delete chassetresor directory

--- chassetresor/src/main/java/fr/cnam/usal3b/nom/prenom/repository/BriqueRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.cnam.usal3b.nom.prenom.repository;
-
-import fr.cnam.usal3b.nom.prenom.model.Brique;
-import org.springframework.data.repository.CrudRepository;
-
-public interface BriqueRepository extends CrudRepository<Brique, Integer> {
-
-}
--- chassetresor/src/main/java/fr/cnam/usal3b/nom/prenom/repository/PlotRepository.java
+++
@@ -1,8 +0,0 @@
-package fr.cnam.usal3b.nom.prenom.repository;
-
-import fr.cnam.usal3b.nom.prenom.model.Plot;
-import org.springframework.data.repository.CrudRepository;
-
-public interface PlotRepository extends CrudRepository<Plot, Integer> {
-
-}
